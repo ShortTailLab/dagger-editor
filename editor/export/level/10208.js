ed5765879ed2d87942df17e834663a6bf1b85e6c
Mao Jingkai <oammix@gmail.com>
2014-02-26 16:16:07 +0800

    editor profiles

---
+++ editor/export/level/10208.js
@@ -0,0 +1,12 @@
+function MAKE_LEVEL(){ var level = {
+	"trigger": [],
+	"behavior": {},
+	"trap": {},
+	"objects": {},
+	"actor": {},
+	"luck": {},
+	"map": {
+		"speed": 100
+	},
+	"bullet": {}
+}; return level; }
